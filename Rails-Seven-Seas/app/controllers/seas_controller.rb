4c58d9e4d868c6e4bebada1882634ef38330d64f
dagpan <panagiotisntagkinis@gmail.com>
2020-02-21 16:02:06 -0500

    Step 1
    Share with Tom

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,7 +1,60 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def welcome
+  end
+  def index
+    @seas = Sea.all
+  end
+  def new
+     @sea = Sea.new
+  end
+  def show
+    @sea = Sea.find(params[:id])
+  end
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+  def create
+    @sea = Sea.new(sea_params)
+    @sea.save
+    redirect_to action: 'show', id: @sea.id
+  end
+  def delete
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+    redirect to "/seas"
+  end
+  def update
+    @sea = Sea.find(params[:id])
+    params.delete("_method")
+    params[:has_mermaids] ||= false
+    @sea.update(sea_params)
+    redirect_to action: 'show', id: @sea.id
+  end
 
+  #   post '/seas', to: "seas#show"
+  #   # patch '/seas/:id', to: "seas#show"
+  #   delete '/seas/:id', to: "seas#index"
 
+  # post "/seas" do
+  #   @sea = Sea.new(params)
+  #   @sea.save
+  #   redirect to "/seas/#{@sea.id}"
+  # end
+
+# patch "/seas/:id" do
+#     @sea = Sea.find(params[:id])
+#     params.delete("_method")
+#     params[:has_mermaids] ||= false
+#     @sea.update(params)
+#     redirect to "/seas/#{@sea.id}"
+#   end
+  
+  # delete "/seas/:id" do
+  #   @sea = Sea.find(params[:id])
+  #   @sea.destroy
+  #   redirect to "/seas"
+  # end
   private
   # In controller actions, use this private method to access sea params from forms.
   # Example: @sea.update(sea_params)
@@ -9,5 +62,5 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
-
+  # In controller actions, use this private method to access sea params from forms.
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+    get '/', to: "seas#welcome", as: 'welcome'
+    get '/seas', to: "seas#index", as: 'seas'
+    get '/seas/new', to: "seas#new", as: 'new'
+    get '/seas/:id', to: "seas#show", as: 'sea'
+    post '/seas', to: "seas#create"
+    get '/seas/:id/edit', to: "seas#edit", as: 'edit'
+    patch '/seas/:id', to: "seas#update"
+    delete '/seas/:id', to: "seas#delete"
+    # match '/seas/:id', to: "seas#show", via: [:get, :patch]
+    # resources :seas, only: [:index, :show, :new, :create, :edit, :update]
+
 end
